7c9a4971e9c217572777daa42ad10a48928e52f3
Rami obaid <147730661+Rami545432@users.noreply.github.com>
2026-04-20 20:07:08 +0200

    added by Jemmy

--- main.js
+++ main.js
@@ -5,3 +5,4 @@ console.log('Fix the error ! ');
 console.log('Here is new branch called javascript_modify!');
 console.log('Here is new branch called javascript_modify!');
 console.log('Here is new branch called javascript_modify!');
+consloe.log(' Added by Jemmy !' );
