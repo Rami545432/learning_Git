4f260828d262f3f6202e65a536004cdcf4acb307
Rami545432 <ramiramos00921@gmail.com>
2026-04-20 20:08:38 +0200

    fixing a typo from jemmy

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ console.log('Fix the error ! ');
 console.log('Here is new branch called javascript_modify!');
 console.log('Here is new branch called javascript_modify!');
 console.log('Here is new branch called javascript_modify!');
-consloe.log(' Added by Jemmy !' );
+console.log(' Added by Jemmy !' );
